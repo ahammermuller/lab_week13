5401166f1b47721b070f4637412ead27f5b57817
ahammermuller <98852797+ahammermuller@users.noreply.github.com>
2022-03-30 10:24:06 -0700

    added a function to app.js to create a replica of the original section inside the copies section

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-function
+//Clone
+
+//Select the div .copy-container and .original-container from index
+const copy_container = document.querySelector('.copy-container');
+const original_section = document.querySelector('.original-container')
+
+//function to append an eventlistener to the element original_section. Test the delegation condition if the event is not inside .copy-container and than insert the element copy.container inside the element, after its last child.
+function copydiv(evt){
+    original_section.addEventListener("click", (evt) => {
+        if (evt.target.tagName != ".copy-container") {
+            copy_container.insertAdjacentHTML("beforeend", evt.target.innerHTML)
+        }
+    })
+}
+copydiv(copy_container)
+
+// Delete
+//function clearcopies(evt){
+    original_section.removeEventListener("click", (evt) => {
+    copy_container.remove()
+    })
