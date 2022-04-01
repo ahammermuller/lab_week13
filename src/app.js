b24e76d66be46c12e7181f5faa538a8854641907
ahammermuller <98852797+ahammermuller@users.noreply.github.com>
2022-03-31 23:20:36 -0700

    added a function to app.js to delete the content from copy-conteiner when the button clear copies is clicked

--- src/app.js
+++ src/app.js
@@ -4,16 +4,22 @@
 const copy_container = document.querySelector('.copy-container');
 const original_section = document.querySelector('.original-container')
 
-//function to append an eventlistener to the element original_section. Test the delegation condition if the event is not inside .copy-container and than insert the element copy.container inside the element, after its last child.
+//function to append an eventlistener to the element original_section. Test the delegation condition if the event target is different from .copy-container and than insert the element copy.container inside the element, after its last child.
 function copydiv(evt){
     original_section.addEventListener("click", (evt) => {
-        if (evt.target.tagName != ".copy-container") {
-            copy_container.insertAdjacentHTML("beforeend", evt.target.innerHTML)
-        }
+    copy_container.insertAdjacentHTML("beforeend", evt.target.innerHTML)
     })
 }
 copydiv(copy_container)
 
 // Delete
-//function clearcopies(evt){
-//    original_section.removeEventListener("click", (evt) => {
+
+//function to append an eventlistener to the button clear copies. The button when clicked delete/returns the HTML content from the element copy_container.
+function clearcopies(evt){
+    const clear_copies = document.querySelector("button");
+    clear_copies.addEventListener("click", (evt) => {
+    copy_container.innerHTML = "";
+    
+    })
+}
+clearcopies(copy_container)
